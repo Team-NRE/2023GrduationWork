7b00ed6fa7a28c67cbe6675f6e681971a892f259
ChangjoSung <tjdckdwh@naver.com>
2022-10-12 19:50:35 +0900

    RE PLAYER MOVE COMMIT

--- Assets/CJ/PlayerMOVE/PlayerMove.cs
+++ Assets/CJ/PlayerMOVE/PlayerMove.cs
@@ -4,53 +4,83 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    float speed;      // 캐릭터 움직임 스피드
-    CharacterController characterController; // 캐릭터 컨트롤러
-    Vector3 movePoint; // 이동 위치 저장
-    Camera mainCamera; // 메인 카메라
+    float speed = 8.0f;
+    public Vector3 movePoint;
+
+    new Rigidbody rigidbody;
+
+    //camera (z,y,속도)
+    Camera viewCamera;
+    [Range(2.0f, 20.0f)]
+    public float Cam_Z = 10.0f;
+
+    [Range(0.0f, 100.0f)]
+    public float Cam_Y = 15.0f;
+    Vector3 velocity = Vector3.zero;
+
+
+    //이동할 위치값
+    Vector3 thisUpdatePoint;
+
+    //업데이트 될 플레이어 각도
+    float rotDegree;
 
     void Start()
     {
-        speed = 4.0f;
-        mainCamera = Camera.main;
-        characterController = GetComponent<CharacterController>();
+        rigidbody = transform.GetComponent<Rigidbody>();
+        viewCamera = Camera.main;
     }
 
     void Update()
     {
-
-        if (Input.GetMouseButtonUp(1))
+        //오른쪽 버튼 누를 시
+        if (Input.GetMouseButtonDown(1))
         {
-            // 카메라에서 레이저를 쏜다.
-            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-            // Scence 에서 카메라에서 나오는 레이저 눈으로 확인하기
-            Debug.DrawRay(ray.origin, ray.direction * 10f, Color.red, 1f);
+            // ray로 마우스 위치 world 좌표로 받기.
+            Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
 
-            // 레이저가 뭔가에 맞았다면
+            //광선 그려주기
+            Debug.DrawRay(ray.origin, ray.direction * 10.0f, Color.green, 1f);
+
             if (Physics.Raycast(ray, out RaycastHit raycastHit))
             {
+                //레어와 닿은 곳이 이동 포인트
                 movePoint = raycastHit.point;
 
+                //각도
+                movePoint.y = 0f;
+                float dx = movePoint.x - rigidbody.position.x;
+                float dz = movePoint.z - rigidbody.position.z;
+                rotDegree = -(Mathf.Rad2Deg * Mathf.Atan2(dz, dx) - 90); //tan-1(dz/dx) = 각도
+
+                Debug.Log("movepoint : " + movePoint.ToString());
             }
         }
-
-        // 목적지까지 거리가 0.1f 보다 멀다면
-        if (Vector3.Distance(transform.position, movePoint) > 0.1f)
-        {
-            // 이동
-            Move();
-        }
-
     }
 
-    void Move()
+    void FixedUpdate()
     {
-        // thisUpdatePoint 는 이번 업데이트(프레임) 에서 이동할 포인트를 담는 변수다.
-        // 이동할 방향(이동할 곳-현재 위치) 곱하기 속도를 해서 이동할 위치값을 계산한다.
-        Vector3 thisUpdatePoint = (movePoint - transform.position).normalized * speed;
-        // characterController 는 캐릭터 이동에 사용하는 컴포넌트다.
-        // simpleMove 는 자동으로 중력을 계산해서 이동시켜주는 메소드다.
-        // 값으로 이동할 포인트를 전달해주면 된다.
-        characterController.SimpleMove(thisUpdatePoint);
+        if (Vector3.Distance(rigidbody.position, movePoint) >= 0.1f)
+        {
+            // thisUpdatePoint 는 이번 업데이트(프레임) 에서 이동할 포인트를 담는 변수다.
+            // 이동할 방향(이동할 곳-현재 위치) 곱하기 속도를 해서 이동할 위치값을 계산한다.
+            thisUpdatePoint = (movePoint - rigidbody.position).normalized * speed;
+
+            //플레이어 이동 및 회전
+            rigidbody.MovePosition(rigidbody.position + thisUpdatePoint * Time.fixedDeltaTime);
+            rigidbody.MoveRotation(Quaternion.Euler(0f, rotDegree, 0f));
+
+            Debug.Log(rotDegree.ToString());
+        }
+    }
+
+    void LateUpdate() {
+        //카메라 이동
+        Vector3 pos = new Vector3(rigidbody.position.x ,rigidbody.position.y + Cam_Y, rigidbody.position.z - Cam_Z);
+        viewCamera.transform.position =  Vector3.SmoothDamp(viewCamera.transform.position, pos, ref velocity, 0.25f);
     }
 }
+
+
+
+
