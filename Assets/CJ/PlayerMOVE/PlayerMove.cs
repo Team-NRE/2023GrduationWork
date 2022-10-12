ddc90d57a0ae0dc3d9cd421e42f37159a3ee03aa
ChangjoSung <tjdckdwh@naver.com>
2022-10-12 19:47:24 +0900

    COMMIT

--- Assets/CJ/PlayerMOVE/PlayerMove.cs
+++ Assets/CJ/PlayerMOVE/PlayerMove.cs
@@ -8,41 +8,41 @@ public class PlayerMove : MonoBehaviour
     CharacterController characterController; // 캐릭터 컨트롤러
     Vector3 movePoint; // 이동 위치 저장
     Camera mainCamera; // 메인 카메라
-    
+
     void Start()
     {
         speed = 4.0f;
         mainCamera = Camera.main;
         characterController = GetComponent<CharacterController>();
     }
-    
+
     void Update()
     {
-    
+
         if (Input.GetMouseButtonUp(1))
         {
             // 카메라에서 레이저를 쏜다.
             Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
             // Scence 에서 카메라에서 나오는 레이저 눈으로 확인하기
             Debug.DrawRay(ray.origin, ray.direction * 10f, Color.red, 1f);
-    
+
             // 레이저가 뭔가에 맞았다면
             if (Physics.Raycast(ray, out RaycastHit raycastHit))
             {
                 movePoint = raycastHit.point;
-    
+
             }
         }
-    
+
         // 목적지까지 거리가 0.1f 보다 멀다면
         if (Vector3.Distance(transform.position, movePoint) > 0.1f)
         {
             // 이동
             Move();
         }
-        
+
     }
-    
+
     void Move()
     {
         // thisUpdatePoint 는 이번 업데이트(프레임) 에서 이동할 포인트를 담는 변수다.
