f7fb86aef25dad6edc8ee32b5329b16b792fd5b0
Checking-pks <69112514+Checking-pks@users.noreply.github.com>
2022-11-16 12:01:01 +0900

    지원봇 사망 처리

--- Assets/PKS/Scripts/objectDeath.cs
+++ Assets/PKS/Scripts/objectDeath.cs
@@ -1,11 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class objectDeath : MonoBehaviour
 {
+    [Header ("- Stats Script")]
+    [SerializeField] Stats stats;
+
+    [Header ("- Components")]
+    [SerializeField] objectController controller;
+    [SerializeField] NavMeshAgent nav;
+    [SerializeField] Animator animator;
+
+    [Header ("- Variable")]
+    [SerializeField] bool isDeath;
+
+    void Start()
+    {
+        // Component 불러오기
+        controller = GetComponent<objectController>();
+        nav = GetComponent<NavMeshAgent>();
+
+        isDeath = false;
+    }
+
+    void FixedUpdate()
+    {
+        if (isDeath && animator.GetCurrentAnimatorStateInfo(0).IsName("Death") && animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.99f)
+            Destroy(gameObject);
+    }
+
     public void Death()
     {
+        controller.enabled = false;
+        nav.enabled = false;
 
+        animator.SetTrigger("Death");
+        animator.SetBool("Move", false);
+        isDeath = true;
     }
 }
