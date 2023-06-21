3929f95a72ca2b03fd7f81e90ac10a100156c43b
Checking-pks <69112514+Checking-pks@users.noreply.github.com>
2023-06-21 22:51:00 +0900

    Update: 미니맵 클릭 시, 카메라 이동 구현. (MiniMap.cs와 Map 스크립트의 Event Trigger 사용)

--- Assets/Script/UI/MiniMap.cs
+++ Assets/Script/UI/MiniMap.cs
@@ -4,6 +4,7 @@
 
 using UnityEngine;
 using UnityEngine.UI;
+using Define;
 
 public class MiniMap : MonoBehaviour
 {
@@ -13,33 +14,59 @@ public class MiniMap : MonoBehaviour
     public float Cam_Z { get; set; }
 
     [SerializeField]
-    Camera miniMapCamera;
     CameraController mainCamera;
-    RawImage mapImage;
-    RawImage cameraFrame;
+    [SerializeField]
+    Canvas canvas;
+    [SerializeField]
+    RectTransform mapRectTransform;
+
+    public bool m_IsButtonDowning;
 
     private void Awake()
     {
         //초기 값 세팅
-        planescale_X = 80; // -80 < X < 80
-        planescale_Z = -4; // -28 < Z < 20 / +24
+        planescale_X = 256f; // -80 < X < 80
+        planescale_Z = 89.6f; // -28 < Z < 20 / +24
         Cam_Y = 9;
         Cam_Z = 6;
 
-        miniMapCamera = gameObject.GetComponentInChildren<Camera>();
         mainCamera = Camera.main.GetComponent<CameraController>();
-        
-        mapImage = transform.Find("Map").GetComponent<RawImage>();
-        cameraFrame = transform.Find("Frame").GetComponent<RawImage>();
     }
 
-    private void Update()
+    void Update()
     {
-        setFramePositionToCameraPosition();
+        if(m_IsButtonDowning)
+        {
+            setFramePositionToCameraPosition();
+        }
+    }
+
+    public void PointerDown()
+    {
+        m_IsButtonDowning = true;
+    }
+
+    public void PointerUp()
+    {
+        m_IsButtonDowning = false;
     }
 
     private void setFramePositionToCameraPosition()
     {
+        Vector2 pos;
+        Vector3 mousePos = Input.mousePosition, newPos;
 
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(mapRectTransform, mousePos, canvas.worldCamera, out pos);
+
+        newPos.x = Mathf.Lerp(-mainCamera.planescale_X, mainCamera.planescale_X, normalizing(-planescale_X, planescale_X, pos.x));
+        newPos.y = mainCamera.transform.position.y;
+        newPos.z = Mathf.Lerp(mainCamera.planescale_Z - 26, mainCamera.planescale_Z + 24, normalizing(-planescale_Z, planescale_Z, pos.y));
+
+        mainCamera.transform.position = newPos;
+    }
+
+    private float normalizing(float min, float max, float value)
+    {
+        return (value - min) / (max - min);
     }
 }
