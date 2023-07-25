c189f22da84348523527ed3e78914790e40f4ad3
Checking-pks <69112514+Checking-pks@users.noreply.github.com>
2023-07-25 17:15:15 +0900

    Add: 로비 페이지 추가

--- Assets/Script/UI/HJH/SliderRunTo1.cs
+++ Assets/Script/UI/HJH/SliderRunTo1.cs
@@ -1,21 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
+
 public class SliderRunTo1 : MonoBehaviour
 {
+	public GameObject loadingText, finishText;
  
      public bool b=true;
 	 public Slider slider;
 	 public float speed=0.5f;
 
-  float time =0f;
-  
-  void Start()
-  {
-	  
-	slider = GetComponent<Slider>();
-  }
+	float time =0f;
+	
+	void Start()
+	{
+		slider = GetComponent<Slider>();
+	}
   
     void Update()
     {
@@ -24,13 +23,14 @@ public class SliderRunTo1 : MonoBehaviour
 			time+=Time.deltaTime*speed;
 			slider.value = time;
 			
-        if(time>1)
-		{
-                    b = false;
-			time=0;
-		}
-    }
+			if(time>1)
+			{
+				b = false;
+				time=0;
+			}
+
+			if (loadingText != null) loadingText.SetActive(b);
+			if (finishText != null) finishText.SetActive(!b);
+    	}
 	}
-	
-	
 }
