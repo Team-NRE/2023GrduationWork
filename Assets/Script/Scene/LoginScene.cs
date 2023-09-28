9c74b1c1b3f8cf6c4486042abc75f8660b36be06
shkim4548 <shkim4548@gmail.com>
2023-09-28 16:09:15 +0900

    게임 재접속 업데이트

--- Assets/Script/Scene/LoginScene.cs
+++ Assets/Script/Scene/LoginScene.cs
@@ -1,12 +1,21 @@
-  using System.Collections;
+using Photon.Pun;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LoginScene : BaseScene
 {
 	
+	private void Awake()
+    {
+		
+    }
+
 	private void Start()
 	{
+		GameObject pm = GameObject.Find("PhotonManager");
+		if (pm == null)
+			Managers.Resource.Instantiate("Prefabs/PhotonManager");
 		Managers.UI.ShowSceneUI<UI_Login>();
 	}
 
