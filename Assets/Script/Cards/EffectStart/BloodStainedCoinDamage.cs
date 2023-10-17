c252f83f62793776fa134a4734de8aac613bc358
shkim4548 <73636884+shkim4548@users.noreply.github.com>
2023-10-17 20:47:57 +0900

    동전 다시만듦

---
+++ Assets/Script/Cards/EffectStart/BloodStainedCoinDamage.cs
@@ -0,0 +1,45 @@
+using Photon.Pun;
+using Stat;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloodStainedCoinDamage : BaseEffect
+{
+    PhotonView _pv;
+
+    float damage;
+    int _playerId;
+    int _targetId;
+    ObjStats oStat;
+    PlayerStats pStat;
+
+    [PunRPC]
+    public override void CardEffectInit(int userId, int targetId)
+    {
+        _pv = GetComponent<PhotonView>();
+        base.CardEffectInit(userId, targetId);
+
+        damage = 10.0f;
+        _playerId = userId;
+        _targetId = targetId;
+
+        this.gameObject.transform.parent = target.transform;
+        this.gameObject.transform.localPosition = new Vector3(0, 0.8f, 0);
+
+        Debug.Log("damage effect");
+        if (target.gameObject.CompareTag("PLAYER"))
+        {
+            pStat = target.GetComponent<PlayerStats>();
+            pStat.GetComponent<PlayerStats>().receviedDamage = (_playerId, damage);
+        }
+        else
+        {
+            oStat = target.GetComponent<ObjStats>();
+            oStat.GetComponent<ObjStats>().nowHealth -= damage;
+        }
+
+        DelayTimer(2.0f);
+        PhotonNetwork.Destroy(this.gameObject);
+    }
+}
